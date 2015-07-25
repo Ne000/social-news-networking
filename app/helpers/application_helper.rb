11e6675416b4dfee36594e2b218b1fe35524712a
staciahoov <staciaray@roadrunner.com>
2015-07-25 15:07:42 +0000

    Checkpoint 42 completed, validating posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def form_group_tag(errors, &block) #& turns block into a Proc object- reusable block
+        if errors.any?
+            content_tag :div, capture(&block), class: 'form-group has-error'
+            #method taks symbol arg.,a block, and options. Creates the symbol-specific html tag with block contents.
+        else
+            content_tag :div, capture(&block), class: 'form-group'
+        end
+    end
 end
+# defining a method that takes two arguments. 1st is an array of errors, 2nd is block
+#
