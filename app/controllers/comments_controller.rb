04cad62686a9cc3c1c5b4613faaa929dfaf71a32
staciahoov <staciaray@roadrunner.com>
2015-07-31 15:57:05 +0000

    progress on checkpoint 46

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,21 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.create(params.require(:comment).permit(:user, :body)) 
-    @comment.post = @post
-    @comment.user = current_user
-    @post = Post.find(params[:post_id])
     @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:post_id])
+    @comment = @post.comments.build(comment_params) 
+    @comment.user = current_user
+    
+    if @comment.save
+      flash[:notice] = "Comment was saved"
+    else
+      flash[:error] =  "Error saving comment"
+    end
+    redirect_to [@topic, @post] 
+  end
+  
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:user, :body)
   end
 end
