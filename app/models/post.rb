6ed1d8532f70e580040f90841a6364ef38e40535
staciahoov <staciaray@roadrunner.com>
2015-08-01 21:54:29 +0000

    progress on checkpoint 49, voting

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
     has_many :comments, dependent: :destroy
     belongs_to :user
     belongs_to :topic
+    has_many :votes, dependent: :destroy
     
     mount_uploader :image, ImageUploader
     
@@ -9,6 +10,6 @@ class Post < ActiveRecord::Base
     
     validates :title, length: { minimum: 5 }, presence: true
     validates :body, length: { minimum: 20 }, presence: true
-    validates :topic, presence: true
-    validates :user, presence: true
+    #validates :topic, presence: true
+    #validates :user, presence: true
 end
