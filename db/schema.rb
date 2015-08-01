b45f9e173acb0541bd1ca6bbd4a9e29bfe2c1c48
staciahoov <staciaray@roadrunner.com>
2015-08-01 17:13:57 +0000

    install rspec, checkpoint 48

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150729154154) do
-=======
+
+
 ActiveRecord::Schema.define(version: 20150729170204) do
->>>>>>> checkpoint-46-comments
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
